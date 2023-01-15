6cc23654e05761e781b86e1435f22519b6927023
Song ji su <thdwltn0727@gmail.com>
2023-01-15 17:31:00 +0900

    Table
    - 팝업 기능 추가!

--- js/scripts.js
+++ js/scripts.js
@@ -215,3 +215,24 @@ for (var i = 0; i < completedTasksHolder.children.length; i ++) {
   //bind events to list item's children (taskCompleted)	
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
+
+var target = document.querySelectorAll('.btn_open');
+var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+var targetID;
+
+// 팝업 열기
+for(var i = 0; i < target.length; i++){
+  target[i].addEventListener('click', function(){
+    targetID = this.getAttribute('href');
+    console.log(targetID);
+    document.querySelector(targetID).style.display = 'block';
+    
+  });
+}
+
+// 팝업 닫기
+for(var j = 0; j < target.length; j++){
+  btnPopClose[j].addEventListener('click', function(){
+    this.parentNode.parentNode.style.display = 'none';
+  });
+}
